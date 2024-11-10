071258a04878cdce8ed3f6e26d512e6fb91582ec
Aliza Moin <160245045+aliza-moin18@users.noreply.github.com>
2024-11-10 10:29:31 -0800

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { FiDownload } from "react-icons/fi"; 
+import { FaLinkedin, FaGithub } from "react-icons/fa"; 
+import Image from "next/image";
+
+const Home = () => {
+  return (
+    <section className="home-section">
+      <div className="container">
+        <div className="content-wrapper">
+          <div className="text-section">
+            <h1>
+              Hello I&apos;m <br />
+              <span className="highlight-text">Aliza Moin</span>
+            </h1>
+            <p>
+              I work as a web developer specializing in creating responsive,
+              beautiful websites. Passionate and meticulous, I transform
+              concepts into powerful digital experiences.
+            </p>
+
+            <div className="action-buttons">
+              <a href="/My CV file.pdf" className="download-btn">
+                <span>Download CV</span>
+                <FiDownload className="icon" />
+              </a>
+
+              <div className="social-links">
+                <a
+                  href="https://www.linkedin.com/in/aliza-moin-b975a6276/" 
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="social-icon"
+                >
+                  <FaLinkedin className="icon" />
+                </a>
+                <a
+                  href="https://github.com/aliza-moin18" 
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="social-icon"
+                >
+                  <FaGithub className="icon" />
+                </a>
+              </div>
+            </div>
+          </div>
+
+         {/* Photo Section */}
+         <div className="photo-section">
+         <Image
+         src="/profile.jpeg" 
+         alt="Aliza Moin"
+         className="profile-image"
+          width={340} 
+          height={340} 
+          />
+         </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
